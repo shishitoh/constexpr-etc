aef9ab4471d96a2f184297def91ef544d6ba6d82
shishitoh <txoeznaon@gmail.com>
2022-08-20 22:48:02 +0900

    commit

--- prime.hpp
+++ prime.hpp
@@ -56,9 +56,97 @@ namespace prime {
         return tuple;
     }
 
-    constexpr auto prime_factorization(std::size_t n) {
-
+    constexpr std::size_t modsum(std::size_t a, std::size_t b, std::size_t n) {
+        return (a == 0 || b < -a) ? (a+b) % n : (a+b-n) % n;
     }
 
+    constexpr std::size_t modprod(std::size_t a, std::size_t b, std::size_t n) {
+        std::size_t ret = 0;
+        while (b) {
+            if (b&1) {
+                ret = modsum(ret, a, n);
+            }
+            a = modsum(a, a, n);
+            b >>= 1;
+        }
+        return ret;
+    }
+
+    constexpr std::size_t modpow(std::size_t a, std::size_t b, std::size_t n) {
+        std::size_t ret = 1;
+        while (b) {
+            if (b&1) {
+                ret = modprod(ret, a, n);
+            }
+            a = modprod(a, a, n);
+            b >>= 1;
+        }
+        return ret;
+    }
+
+    /* n = 2^s*dを満たす */
+    constexpr bool miller_rabin_core(std::size_t n, std::size_t a, std::size_t s, std::size_t d) {
+
+        std::size_t y = modpow(a, d, n);
+        if (y == 1) {
+            return true;
+        }
+        for (std::size_t r = 0; r < s; ++r) {
+            if (y == n-1) {
+                return true;
+            }
+            y = modprod(y, y, n);
+        }
+        return false;
+    }
+
+    constexpr is_prime(std::size_t n) {
+
+        if (n == 2) {
+            return true;
+        } else if (n==0 || n==1 || !(n&1)) {
+            return false;
+        }
+
+        std::size_t A[7];
+        std::size_t As;
+
+        if (n < 1373653ULL) {
+            A[0] = 2; A[1] = 3; As = 2;
+        } else if (n < 25326001ULL) {
+            A[0] = 2; A[1] = 3; A[2] = 5; As = 3;
+        } else if (n < 118670087467) {
+            A[0] = 2; A[1] = 3; A[2] = 5; A[3] = 7;
+            As = 4;
+        } else if (n < 2152302898747ULL) {
+            A[0] = 2; A[1] = 3; A[2] = 5; A[3] = 7;
+            A[4] = 11; As = 5;
+        } else if (n < 3474749660383ULL) {
+            A[0] = 2; A[1] = 3; A[2] = 5; A[3] = 7;
+            A[4] = 11; A[5] = 13; As = 6;
+        } else if (n < 341550071728321ULL) {
+            A[0] = 2; A[1] = 3; A[2] = 5; A[3] = 7;
+            A[4] = 11; A[5] = 13; A[6] = 17; As = 7;
+       } else {
+           /* not implemented */
+            return false;
+        }
+
+        std::size_t d = n-1;
+        std::size_t s = 0;
+        while (!(d&1)) {
+            ++s;
+            d >>= 1;
+        }
+        for (std::size_t Ass = 0; Ass < As; ++Ass) {
+            if (A[Ass] >= n) {
+                break;
+            }
+            if (!miller_rabin_core(n, A[Ass], s, d)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
--- test/prime.cpp
+++ test/prime.cpp
@@ -9,11 +9,19 @@ int main() {
     constexpr std::size_t pmax = 100;
     constexpr auto tuple = tuple_sieve<pmax>();
     static_assert(
-        tuple_sieve<100>() ==
-            (decltype(tuple)){
-                2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-                31, 37, 41, 43, 47, 53, 59, 61, 67,
-                71, 73, 79, 83, 89, 97});
-
+        tuple_sieve<100>() == (decltype(tuple)){
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+            31, 37, 41, 43, 47, 53, 59, 61, 67,
+            71, 73, 79, 83, 89, 97});
+    static_assert(is_prime(1) == false);
+    static_assert(is_prime(2) == true);
+    static_assert(is_prime(3) == true);
+    static_assert(is_prime(61) == true);
+    static_assert(is_prime(275) == false);
+    static_assert(is_prime(44973563) == true);
+    static_assert(is_prime(449735651) == true);
+    static_assert(is_prime(1234567891) == true);
+    static_assert(is_prime(1592644381) == false);
+    static_assert(is_prime(111111111111229) == true);
 }
 
